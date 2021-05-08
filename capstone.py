d20f53225f19b8d8586e91e1dffebc842354fd7a
JessicaLuo <maginohala@gmail.com>
2021-05-08 23:21:37 +0800

    create a python script for assignment

---
+++ capstone.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  8 22:36:20 2021
+
+@author: Jessicaluo
+"""
+
+
+#This notebook will be mainly used for the capstone project
+
+
+import pandas as pd
+import numpy as np
+print ("Hello Capstone Project Course!") 
